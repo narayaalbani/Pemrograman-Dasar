24a9b5b8bdf3431d405408f37f5ada10e356dbbc
narayaalbani <naraya.albani@gmail.com>
2023-11-07 11:24:32 +0700

    Membuat program rumus luas permukaan dan volume bangun ruang

--- src/RumusBangunRuang.java
+++ src/RumusBangunRuang.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class RumusBalok {
+public class RumusBangunRuang {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         double pi = Math.PI;
